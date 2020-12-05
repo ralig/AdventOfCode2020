c9aef26e28ee6a37042dcfac6edef7c62fc1b811
Chris Martin cd 'c:\Users\ralig\AdventOfCode2019'; & 'C:\Users\ralig\AppData\Local\Programs\Python\Python39\python.exe' 'c:\Users\ralig\.vscode\extensions\ms-python.python-2020.11.371526539\pythonFiles\lib\python\debugpy\launcher' '64768' '--' 'c:\Users\ralig\AdventOfCode2019\19-3-1.py <ralig98@gmail.com>
2020-12-05 00:40:23 -0600

    Day 5

---
+++ Day 5/Python/Part 2/solve.py
@@ -0,0 +1,28 @@
+import math
+from pathlib import Path
+path = Path(__file__).parent / "../../input.txt"
+
+def FindRowOrCol(minSeatNum, maxSeatNum, info):
+    if info == "":
+        return minSeatNum
+    elif (info[0] == "F") or (info[0] == "L"):
+        info2 = info[1:]
+        return FindRowOrCol(minSeatNum,((minSeatNum+maxSeatNum)//2),info2)
+    elif (info[0] == "B") or (info[0] == "R"):
+        info2 = info[1:]
+        return FindRowOrCol(math.ceil((minSeatNum+maxSeatNum)/2),maxSeatNum,info2)
+    
+
+Seats = []
+with path.open("rt") as f:
+    data = "a"
+    while(data):
+        data = f.readline().replace("\n","")
+        row = FindRowOrCol(0, 127, data[:-3])
+        col = FindRowOrCol(0, 7, data[-3:])
+        Seats.append((row*8)+col)
+
+Seats.sort()
+for x in range(1,len(Seats)-1):
+    if ((Seats[x] + 1) != Seats[x+1]):
+        print(Seats[x]+1)
